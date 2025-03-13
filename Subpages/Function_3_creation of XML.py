d9b4d6aecf0598196eecc53595df454175040830
netj22693 <netolicka.jan@email.cz>
2025-03-13 12:15:17 +0000

    13-Mar-2025 - Function 3 - core logic done

---
+++ Subpages/Function_3_creation of XML.py
@@ -0,0 +1,371 @@
+import streamlit as st
+import xml.etree.ElementTree as ET
+import string
+import random
+import time
+
+
+
+# Function for pretty print of XML elements
+def prettify(element, indent='  '):
+    queue = [(0, element)]  # (level, element)
+    while queue:
+        level, element = queue.pop(0)
+        children = [(level + 1, child) for child in list(element)]
+        if children:
+            element.text = '\n' + indent * (level+1)  # for child open
+        if queue:
+            element.tail = '\n' + indent * queue[0][0]  # for sibling open
+        else:
+            element.tail = '\n' + indent * (level-1)  # for parent close
+        queue[0:0] = children  # prepend so children come before siblings
+
+
+
+# Function for generating random 6 digits for invoice number
+def id_generator(size=6, chars=string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+a = id_generator()
+invoice_number_generated = 'INV-' + a
+
+
+
+# Generation of date for <date> element
+now = time.localtime()
+print(now)
+date_input = time.strftime("%Y-%m-%d", now)
+
+with st. expander("Buyer"):
+    customer_input = st.text_input(
+        "Customer:",
+        help = "Write a customer name"
+        )
+
+with st. expander("Product"):
+
+    product_name_inp = st.text_input(
+        "Product name:",
+        help = "Write a product name"
+        )
+
+    category_selb = st.selectbox(
+        "Category" ,
+        index = None,
+        placeholder= "Select...",
+        options=["PC","TV","Gaming","Mobile phones","Tablets","Major Appliances","Households"],
+        help = "Select category from which the product is"
+         )
+
+with st. expander("Price"):
+    currency_selb = st.selectbox(
+        "Currency" ,
+        index = None,
+        placeholder= "Select...",
+        options=["euro","US dollar","Kč"],
+        help = "Select one from the predefined currencies"
+        )
+    
+    price = st.number_input(
+        "Price",
+        min_value=0.00,
+        step = 10.00,
+        help = "You can either click on the +- icons or write the input using numbers. *The step is step +- 10.00 -> i case of diferent values in decimals wrrte it."
+        )
+
+
+with st.expander("Extra purchase"):
+
+    add_service_select = st.selectbox(
+        "Additional service" ,
+        options=["No additional service","Insurance","Extended varanty"],
+        help = "Select one of the options"
+         )
+
+    if add_service_select == 'Insurance':
+        st.info("costs 15% from product price")
+    
+    if add_service_select == 'Extended varanty':
+        st.info("costs 10% from product price")
+
+with st.expander("Transportation"):
+
+    city_selb = st.selectbox(
+        "Select city for delivery" ,
+        options=["Prague","Berlin"],
+        index = None,
+        placeholder="Select...",
+        help = "Select one of the options - there is different price for service for each city"
+         )
+      
+    transport_co_selb = st.selectbox(
+        "Transporting compeny" ,
+        options=["DHL","Fedex"],
+        index = None,
+        placeholder="Select...",
+        help = "Select one of the options - there is different price for each company"
+         )
+    
+    size_selb = st.selectbox(
+        "Size of package" ,
+        options=["small","medium","large"],
+        index = None,
+        placeholder="Select...",
+        help = "Select one of the options - there is different price for each size"
+        )
+    
+    st.image("Pictures/Function_3/Price_list.png")
+
+price_str = str(price)
+
+# logika pro transport
+
+def calculation_transport(city_selb,size_selb,transport_co_selb, currency_selb):   
+    # Prague, DHL, small
+    if city_selb == 'Prague' and transport_co_selb == 'DHL' and size_selb == 'small' and currency_selb == 'euro':
+        return 2.00
+    
+    if city_selb == 'Prague' and transport_co_selb == 'DHL' and size_selb == 'small' and currency_selb == 'US dollar':
+        return 2.17
+    
+    if city_selb == 'Prague' and transport_co_selb == 'DHL' and size_selb == 'small' and currency_selb == 'Kč':
+        return 50.00
+
+    # Prague, DHL, medium
+    if city_selb == 'Prague' and transport_co_selb == 'DHL' and size_selb == 'medium' and currency_selb == 'euro':
+        return 3.20
+    
+    if city_selb == 'Prague' and transport_co_selb == 'DHL' and size_selb == 'medium' and currency_selb == 'US dollar':
+        return 3.48
+    
+    if city_selb == 'Prague' and transport_co_selb == 'DHL' and size_selb == 'medium' and currency_selb == 'Kč':
+        return 80.00
+
+    # Prague, DHL, large
+    if city_selb == 'Prague' and transport_co_selb == 'DHL' and size_selb == 'large' and currency_selb == 'euro':
+        return 4.00
+    
+    if city_selb == 'Prague' and transport_co_selb == 'DHL' and size_selb == 'large' and currency_selb == 'US dollar':
+        return 4.35
+    
+    if city_selb == 'Prague' and transport_co_selb == 'DHL' and size_selb == 'large' and currency_selb == 'Kč':
+        return 100.00
+    
+    # Prague, Fedex, small
+    if city_selb == 'Prague' and transport_co_selb == 'Fedex' and size_selb == 'small' and currency_selb == 'euro':
+        return 2.40
+    
+    if city_selb == 'Prague' and transport_co_selb == 'Fedex' and size_selb == 'small' and currency_selb == 'US dollar':
+        return 2.61
+    
+    if city_selb == 'Prague' and transport_co_selb == 'Fedex' and size_selb == 'small' and currency_selb == 'Kč':
+        return 60.00
+    
+    # Prague, Fedex, medium
+    if city_selb == 'Prague' and transport_co_selb == 'Fedex' and size_selb == 'medium' and currency_selb == 'euro':
+        return 3.20
+    
+    if city_selb == 'Prague' and transport_co_selb == 'Fedex' and size_selb == 'medium' and currency_selb == 'US dollar':
+        return 3.48
+    
+    if city_selb == 'Prague' and transport_co_selb == 'Fedex' and size_selb == 'medium' and currency_selb == 'Kč':
+        return 80.00
+    
+    # Prague, Fedex, large
+    if city_selb == 'Prague' and transport_co_selb == 'Fedex' and size_selb == 'large' and currency_selb == 'euro':
+        return 4.40
+    
+    if city_selb == 'Prague' and transport_co_selb == 'Fedex' and size_selb == 'large' and currency_selb == 'US dollar':
+        return 4.78
+    
+    if city_selb == 'Prague' and transport_co_selb == 'Fedex' and size_selb == 'large' and currency_selb == 'Kč':
+        return 110.00
+    
+    # Berlin, DHL, small
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'small' and currency_selb == 'euro':
+        return 2.80
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'small' and currency_selb == 'US dollar':
+        return 3.04
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'small' and currency_selb == 'Kč':
+        return 70.00
+    
+    # Berlin, DHL, medium
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'medium' and currency_selb == 'euro':
+        return 4.00
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'medium' and currency_selb == 'US dollar':
+        return 4.35
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'medium' and currency_selb == 'Kč':
+        return 100.00
+    
+    # Berlin, DHL, small
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'small' and currency_selb == 'euro':
+        return 2.80
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'small' and currency_selb == 'US dollar':
+        return 3.04
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'small' and currency_selb == 'Kč':
+        return 70.00
+    
+    # Berlin, DHL, large
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'large' and currency_selb == 'euro':
+        return 6.00
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'large' and currency_selb == 'US dollar':
+        return 6.52
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'DHL' and size_selb == 'large' and currency_selb == 'Kč':
+        return 150.00
+
+    # Berlin, Fedex, small
+    if city_selb == 'Berlin' and transport_co_selb == 'Fedex' and size_selb == 'small' and currency_selb == 'euro':
+        return 2.60
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'Fedex' and size_selb == 'small' and currency_selb == 'US dollar':
+        return 2.83
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'Fedex' and size_selb == 'small' and currency_selb == 'Kč':
+        return 65.00
+    
+    # Berlin, Fedex, medium
+    if city_selb == 'Berlin' and transport_co_selb == 'Fedex' and size_selb == 'medium' and currency_selb == 'euro':
+        return 4.80
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'Fedex' and size_selb == 'medium' and currency_selb == 'US dollar':
+        return 5.22
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'Fedex' and size_selb == 'medium' and currency_selb == 'Kč':
+        return 120.00
+    
+    # Berlin, Fedex, large
+    if city_selb == 'Berlin' and transport_co_selb == 'Fedex' and size_selb == 'large' and currency_selb == 'euro':
+        return 6.00
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'Fedex' and size_selb == 'large' and currency_selb == 'US dollar':
+        return 6.52
+    
+    if city_selb == 'Berlin' and transport_co_selb == 'Fedex' and size_selb == 'large' and currency_selb == 'Kč':
+        return 150.00
+
+calc_transport_price = calculation_transport(city_selb,size_selb,transport_co_selb, currency_selb)
+
+# logika pro additional service
+
+def fun_add_service_1(option):
+    
+    if option == 'No additional service':
+        return 'None'
+
+    elif option == 'Insurance':
+        return 'insurance'
+    
+    elif option == 'Extended varanty':
+        return 'extended varanty'
+
+service_type_fn = fun_add_service_1(add_service_select)
+
+def fun_add_service_2(option, price):
+    
+    if option == 'No additional service':
+        return '0.00'
+
+    elif option == 'Insurance':
+        ins = price * 0.15
+        #ins = str(ins)
+        return ins
+    
+    elif option == 'Extended varanty':
+        extv = price * 0.1
+        #extv = str(extv)
+        return extv
+
+service_price_fn = fun_add_service_2(add_service_select, price)
+service_price_fn_str = str(service_price_fn)
+
+
+def fun_add_service_3(option):
+    
+    if option == 'No additional service':
+        return 'N'
+
+    else:
+        return 'Y'
+       
+
+service_fn = fun_add_service_3(add_service_select)
+
+
+if customer_input == '' or product_name_inp == '' or category_selb == '' or currency_selb == '' or price == '' or add_service_select == '':
+    st.warning("One of the inputs is still not available")
+
+else:
+    st.success("Fulfiled properly")
+
+# Calculation of final price 
+final_price_fl = price + calc_transport_price + service_price_fn
+final_price_fl_str = str(final_price_fl)
+
+if st.button("submit"):
+    st.write("Jane ty jsi genius")
+    
+
+    st.write(f" - Customer name: {customer_input}")
+    ''
+    st.write(f" - Product name: {product_name_inp}")
+    st.write(f" - Category: {category_selb}")
+    st.write(f" - Price: {price:.2f} {currency_selb}")
+    ''
+    st.write(f" - Extra service: {add_service_select}")
+    st.write(f" - Price for the extra service: {service_price_fn:.2f} {currency_selb}")
+    ''
+    st.write(f" - Price transport: {calc_transport_price:.2f} {currency_selb}")
+    ''
+    st.write(f" - Total price to pay: {final_price_fl:.2f} {currency_selb}")
+
+    st.info("If this is what you expect you can proceed with Download button which will create XML file. If not, you can go up and change your inputs and then use the Submit button again.")
+    
+    # Change of data type
+    calc_transport_price_str = str(calc_transport_price)
+       
+   
+    xml_doc = ET.Element("invoice")
+    header = ET.SubElement(xml_doc, 'header')
+    order_number = ET.SubElement(header, 'order_number')
+    customer = ET.SubElement(header, 'customer').text = customer_input
+    invoice_number = ET.SubElement(header, 'invoice_number').text = invoice_number_generated
+    date = ET.SubElement(header, 'date').text = date_input
+    price = ET.SubElement(header, 'price')
+    total_sum = ET.SubElement(price, 'total_sum').text = final_price_fl_str
+    #total_sum_services = ET.SubElement(price, 'total_sum_services').text = service_price_fn
+    currency = ET.SubElement(price, 'currency').text = currency_selb
+
+    detail = ET.SubElement(xml_doc, 'detail')
+    category= ET.SubElement(detail, 'category').text = category_selb
+    product_name = ET.SubElement(detail, 'product_name').text = product_name_inp
+    price_amount = ET.SubElement(detail, 'price_amount').text = price_str
+    addtional_service = ET.SubElement(detail, 'additional_service')
+    service = ET.SubElement(addtional_service, 'service'). text = service_fn
+    service_type = ET.SubElement(addtional_service, 'service_type').text = service_type_fn
+    service_price = ET.SubElement(addtional_service, 'service_price').text = service_price_fn_str
+
+    transportation = ET.SubElement(xml_doc, 'transportation')
+    transporter = ET.SubElement(transportation, 'transporter').text = transport_co_selb
+    to_city = ET.SubElement(transportation, 'to_city').text = city_selb
+    size = ET.SubElement(transportation, 'size').text = size_selb
+    transport_price = ET.SubElement(transportation, 'transport_price').text = calc_transport_price_str
+    prettify(xml_doc)
+
+    tree = ET.ElementTree(xml_doc)
+    # xml_declaration=Tru -> generuje XML prolog
+    tree.write('Data/Function_3_do NOT delete.xml', encoding='UTF-8', xml_declaration=True)
+ 
+
+file_name_fstring = f"{invoice_number_generated}.xml"
+
+with open('Data/Function_3_do NOT delete.xml') as f:
+    if st.download_button('Download', f, file_name = file_name_fstring):
+        st.info("download will start in few seconds")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,62 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# --- SHARED ON ALL PAGES ---
+# st.logo("Pictures/Logo2.png", size='large')
+
+
+# Pages as objects
+# testing = st.Page(
+#     "Subpages/testuju.py",
+#     title="TESTING"
+#     )
+
+# app_purpose = st.Page(
+#     "Subpages/Purpose_of_app.py",
+#     title="Purpose of this application"
+#     )
+
+# app_description_ArM = st.Page(
+#     "Subpages/application_description_archimate.py",
+#     title="Description Archimate"
+#     )
+
+# app_description_BPMN = st.Page(
+#     "Subpages/application_description_BPMN.py",
+#     title="Description BPMN"
+#     )
+
+# download = st.Page(
+#     "Subpages/XML_dowload.py",
+#     title="1. XML - Download"
+#     )
+
+# xsd = st.Page(
+#     "Subpages/XML_XSD_schema.py",
+#     title="Description - XSD, XML Schema"
+#     )
+
+# parsing = st.Page(
+#     "Subpages/XML_parsing_to_txt_outcome.py",
+#     title="2. XML - Parsing, Validation, Vizualization"
+#     )
+
+function_3 = st.Page(
+    "Subpages/Function_3_creation of XML.py",
+    title="3. XML - Creation of XML"
+    )
+
+
+# Navigation:
+pg = st.navigation(
+    {
+        #"About this application": [app_purpose , app_description_ArM , app_description_BPMN , xsd],
+        #"TESTING": [testing],
+        "Application functions": [
+            #download,
+            #parsing,
+            function_3,
+            ]
+    }
 )
+
+pg.run()
