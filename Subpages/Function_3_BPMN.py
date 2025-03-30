fe4d227c28b6dab277cd65a29bdfe67ee3133e27
netj22693 <netolicka.jan@email.cz>
2025-03-30 15:57:08 +0000

    30-Mar-2025: typos

--- Subpages/Function_3_BPMN.py
+++ Subpages/Function_3_BPMN.py
@@ -16,7 +16,7 @@ Inputs to be entered by user and submited:
 st.image("Pictures/Function_3/delivery details.png", width = 200)
 ''
 '''
-Then application/function makes the calculation. In case of all inputs are okay, user can push download button for generating of invoice either in XML format or JSON. In case that user wants to change something in the original inputs, he can rewrite/change the inputs and then push Submit button again...
+Then application/function makes the calculation. In case of all inputs are okay, user can push download button for generating of an invoice either in XML format or JSON. In case that user wants to change something in the original inputs, he can rewrite/change the inputs and then push Submit button again...
 '''
 st.write("-----")
 st.write("##### Application calculation process:")
@@ -25,10 +25,16 @@ st.write("##### Application calculation process:")
 st.image("Pictures/Function_3/Function_3_BPMN_calculation process_2.png")
 ''
 '''
-The calculation happens based on predefined inputs/costs which are part of application rules in the code. If the BPMN diagram would be transfared into the "application look" then it would be like this:
+The calculation happens based on predefined inputs/costs which are part of application rules in the code.
+'''
+'''
+If the BPMN diagram would be transfared into the "application look" then it would be like this:
 '''
 ''
+''
 st.image("Pictures/Function_3/Function_3_BPMN_calculation process_tables_2.png")
+''
+''
 ''
 '''
 The predefined values for calculations are:
@@ -47,7 +53,7 @@ st.write("#### Function 4 process flow:")
 ''
 ''
 '''
-In case that user wants to change file format of invoice generated in Function 3 step there is a possibility to use Function 4 for mapping into the other format than has been selected. Reason why to do this? The existing inovice file from Function 3 has uniquie ID and order number specific for the invoice and thus this conversion into the other file format will keep these unique IDs.
+In case that user wants to change file format of invoice generated in Function 3 step, there is a possibility to use Function 4 for mapping into the other format than has been selected. Reason why to do this and not generate new one? The existing inovice file from Function 3 has uniquie ID and order number specific for the invoice and thus this conversion into the other file format will keep these unique IDs.
 '''
 ''
 ''
