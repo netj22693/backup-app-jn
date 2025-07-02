8fa0144af9a875f0a927e6f94822ab37d49cb931
netj22693 <netolicka.jan@email.cz>
2025-07-02 08:20:47 +0000

    02-July-2025 -v9.2 - bug form added

--- Subpages/Purpose_of_app.py
+++ Subpages/Purpose_of_app.py
@@ -117,12 +117,41 @@ st.page_link(
 	icon=":material/play_circle:"
 	) 
 
+# Bug - expander
+''
+''
+''
+with st.expander("Have you seen a bug? Report it here.",icon= ":material/pest_control:"):
+
+    ''
+    st.write("Please provide details:")
+
+    contact_form ="""
+        <form action="https://formsubmit.co/honza.ne@seznam.cz" method="POST">
+            <input type="hidden" name="_captcha" value="false">
+            <input type="text" name="subject" placeholder= "Subject" required>
+            <textarea name="message" placeholder="Description..."></textarea>
+            <button type="submit">Send</button>
+        </form>
+    """
+
+    st.markdown(contact_form, unsafe_allow_html = True)
+
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+    local_css("Subpages/CSS/style.css")
+
+    ''
+    ''
+    st.caption("Powered by FormSubmit")
+    st.image("Pictures/formsubmitlogo.png", width=150)
+
 
 
 # Release notes - expander
-''
-''
-''
+
 with st.expander(
 	"Release notes",
 	icon=":material/event_note:"
--- Subpages/testuju.py
+++ Subpages/testuju.py
@@ -1,21 +1,29 @@
 import streamlit as st
 
 
-with st.expander("Cau"):
-    st.write("cus")
+with st.expander("Have you seen a bug? Report it here.",icon= ":material/pest_control:"):
+
+    ''
+    st.write("Please provide details:")
 
     contact_form ="""
         <form action="https://formsubmit.co/honza.ne@seznam.cz" method="POST">
-            <input type="text" name="name" required>
-            <input type="email" name="email" required>
+            <input type="hidden" name="_captcha" value="false">
+            <input type="text" name="subject" placeholder= "Subject" required>
+            <textarea name="message" placeholder="Description..."></textarea>
             <button type="submit">Send</button>
         </form>
     """
 
     st.markdown(contact_form, unsafe_allow_html = True)
 
-    # def local_css(file_name):
-    #     with open(file_name) as f:
-    #         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-    # local_css("Subpages/CSS/style.css")
+    local_css("Subpages/CSS/style.css")
+
+    ''
+    ''
+    st.caption("Powered by FormSubmit")
+    st.image("Pictures/formsubmitlogo.png", width=150)
