1568aac52415a04e50a0c2d3e33b4674a0419e10
netj22693 <netolicka.jan@email.cz>
2025-08-12 07:43:48 +0000

    12-Aug-2025 -v20.5 - Archimate adjustment

--- Subpages/Purpose_of_app.py
+++ Subpages/Purpose_of_app.py
@@ -49,7 +49,7 @@ with tab1:
 	st.write("###### ArchiMate - Overview of the functions:")
 	''
 	# st.image("Pictures/Archimate_functions_overview.svg")
-	st.image("Pictures/Archimate_functions_overview_4.svg")
+	st.image("Pictures/Archimate_functions_overview_5.svg")
 	
 with tab2:
 	st.write("###### UML - Overview of the functions:")
