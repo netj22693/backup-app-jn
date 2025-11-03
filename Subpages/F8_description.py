177ef35e3d903183b97219a76281643375fcc295
netj22693 <netolicka.jan@email.cz>
2025-11-03 16:19:51 +0000

    03-Nov-2025 -v36.5 F8 description extended

--- Subpages/F8_description.py
+++ Subpages/F8_description.py
@@ -11,7 +11,6 @@ st.write("""
 ''
 ''
 
-st.info("This section is still under build - This is just basic view - more will be provided soon :)")
 
 st.write("##### Application environment:")
 ''
@@ -33,13 +32,46 @@ st.write("##### DB structure - ERD:")
 ''
 st.write("""
 - The DB is designed based on **principle of 2 kinds of tables**
-- Table 1 **'company'** stores information about companies providing transport - and what type of transport
+- Table 1 **'company'** stores information about companies providing transport - and what type of transport does - **Truck, Train, Airplane, International/Domestic transport** 
 - Table 2 **'country_XX**' stores information about branches/contact points of the companies per specific country
-- This creates a relationship **1 Parent - multiple Children** having **'1 to Many'** relations also when it comes to records
+- This creates a relationship **1 Parent - multiple Children** having **'1 to Many Optional'** relations also when it comes to records
 """)
 ''
 st.image("Pictures/Function_8/F8_ERD.svg")
 
+''
+st.write("""
+- When a **new company onboarded**, it gets created in the **'company'** table and **PostgreSQL will generate 'comp_id'** which is **PK** and **unique identifier** for the company/record in the DB
+- Then **branches of the company** can be added to **'country_XX'** table(s), depending in which of the countries there are any of them
+	- **'c_comp_id'** - is **FK** and it is the id of the company as was given in 'company' table
+	- when new record added, **PostgreSQL will provide next available 'branch_id' to the record**. The benefit of PostgreSQL here - it takes the last number checking all **'country_XX'** tables -> there is not possible to have two times the same 'branch_id' -> **a record gets unique identifier always** across **all** the tables
+""")
+''
+''
+st.image("Pictures/Function_8/F8_erd_small.svg")
+
+
+''
+st.write("""
+- With this DB logic there can be SQL queries set in the code to provide relevant data to the user screen
+""")
+
+''
+st.write("""
+- User is searching for companies available for particular type of transport (Truck, Train, Airplane) and Internation/Domestic transport
+""")
+
+''
+st.image("Pictures/Function_8/F8_bpmn_query_internal_crossborder.svg")
+
+
+''
+st.write("""
+- User is for particular company branches 
+""")
+
+''
+st.image("Pictures/Function_8/F8_bpmn_query_branches.svg")
 
 
 # ===== Page navigation at the bottom ======
