e5a9b94febe7dfdda3834b8180d871e3e52d2e4d
netj22693 <netolicka.jan@email.cz>
2026-03-16 15:56:29 +0000

    16-Mar-2026 -v55.1 - F7 & F7B - UI image generator (images adjustment, code extended by types) + text fix on main page

--- Subpages/F7_UI_image_generator.py
+++ Subpages/F7_UI_image_generator.py
@@ -1,6 +1,9 @@
 import streamlit as st 
 
-def provide_ui_image_path(transport, dtd_a, dtd_b, truck_breaks):
+def provide_ui_image_path(transport: str, dtd_a: int, dtd_b: int, truck_breaks: float) -> str:
+    '''
+    Based on variables/inputs generates path to the relevant image
+    '''
 
     a = "y" if dtd_a > 0 else "n"
     b = "y" if dtd_b > 0 else "n"
@@ -11,7 +14,10 @@ def provide_ui_image_path(transport, dtd_a, dtd_b, truck_breaks):
     return path
 
 
-def provide_ui_color_coding_image(transport, dtd_a, dtd_b, truck_breaks):
+def provide_ui_color_coding_image(transport: str, dtd_a: int, dtd_b: int, truck_breaks: float) -> str:
+    '''
+    Based on variables/inputs generates path to the relevant image
+    '''
 
     a = "y" if dtd_a > 0 else "n"
     b = "y" if dtd_b > 0 else "n"
@@ -37,5 +43,4 @@ def provide_ui_color_coding_image(transport, dtd_a, dtd_b, truck_breaks):
 
     path = f"Pictures/Function_7/F7_transport_flow_images/F7B_color_coding_{transport}_dtd_A_{a}{ta}_dtd_B_{b}{tb}{breaks}.svg"
     
-
     return path
