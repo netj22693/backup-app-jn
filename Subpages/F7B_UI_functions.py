c5e7f1277b87542ed3a2d13183c9e14c5cdea30a
netj22693 <netolicka.jan@email.cz>
2026-08-10 09:54:17 +0000

    10-Aug-2026 -v67.5.4 - F7B image fix

--- Subpages/F7B_UI_functions.py
+++ Subpages/F7B_UI_functions.py
@@ -5,11 +5,11 @@ import streamlit as st
 def display_state_flow():
 
     st.write("") # Workaround for space on UI 
-    st.image("Pictures/Function_7/F7B_state_flow/F7B_state_flow_v1.svg", width = 680)
+    st.image("Pictures/Function_7/F7B_state_flow/F7B_state_flow_v2.svg", width = 680)
 
 def display_state_flow_expander():
 
     with st.expander("State flow", icon=":material/info:"):
-        st.image("Pictures/Function_7/F7B_state_flow/F7B_state_flow_v1.svg", width = 580)
+        st.image("Pictures/Function_7/F7B_state_flow/F7B_state_flow_v2.svg", width = 580)
         st.write("") # Workaround for space on UI
         st.write(" - For manual **Approve/Reject** - Go to **Search for specific offer** tab and search for the offer.")
